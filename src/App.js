4c6deb878115458a60a0f61e1c2db6c6ca7a8c12
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-01 14:47:58 +0200

    Added route to CartPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 import ProductPage from "./pages/ProductPage";
 import { Switch, Route } from "react-router-dom";
-import "./App.css";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
     <div>
       <Switch>
+        <Route path="/cartpage">
+          <CartPage />
+        </Route>
         <Route path="/">
           <ProductPage />
         </Route>
