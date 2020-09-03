90271a6469de7747d0a40f6223dcf1acf372c910
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-03 14:50:45 +0200

    just prettier changes

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,41 +1,36 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import ProductDetails from '../components/ProductDetails'
+import React from "react";
+import { useEffect, useState } from "react";
+import ProductDetails from "../components/ProductDetails";
 
-export default function ProductDetailPage({props}) {
-    let [productData, setProductData] = useState({})
-    let ProductId = props.match.params.id;
+export default function ProductDetailPage({ props }) {
+  let [productData, setProductData] = useState({});
+  let ProductId = props.match.params.id;
 
-    function fetchImage() {
-      fetch(`https://mock-data-api.firebaseio.com/e-commerce.json`)
-        .then(res => res.json())
-        .then(result => {
-            
-            setProductData(result.products[ProductId])
+  function fetchImage() {
+    fetch(`https://mock-data-api.firebaseio.com/e-commerce.json`)
+      .then(res => res.json())
+      .then(result => {
+        setProductData(result.products[ProductId]);
+      });
+  }
 
-        })
-        
-    }
-  
-    useEffect(() => {
-      fetchImage()
+  useEffect(() => {
+    fetchImage();
+  }, []);
 
-    }, [])
+  useEffect(() => {}, [productData]);
 
-    useEffect(() => {
-      }, [productData])
-
-    return (
-      <div>
-        <h1>Image Detail Page</h1>
-        <ProductDetails
-        name= {productData.name}
-        description= {productData.description}
-        price= {productData.price}
-        stock= {productData.stock}
-        rating= {productData.rating}
-        imageArr= {productData.images}
-        />
-      </div>
-    )
+  return (
+    <div>
+      <h1>Image Detail Page</h1>
+      <ProductDetails
+        name={productData.name}
+        description={productData.description}
+        price={productData.price}
+        stock={productData.stock}
+        rating={productData.rating}
+        imageArr={productData.images}
+      />
+    </div>
+  );
 }
