d4d6999da0b871484917daa444f8f42ccd4da608
Lova Duvnäs <lova.duvnas@yh.nackademin.se>
2020-09-03 11:15:41 +0200

    Added shortDiscription to ProductCard

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,8 +7,8 @@ export default function ProductList() {
 
   function fetchProductList() {
     fetch("https://mock-data-api.firebaseio.com/e-commerce/products.json")
-      .then((res) => res.json())
-      .then((result) => {
+      .then(res => res.json())
+      .then(result => {
         console.log(result);
         setProductList(result);
       });
@@ -28,11 +28,12 @@ export default function ProductList() {
 
               return (
                 <div className="col-sm-4">
-				{/* TODO: Add key attribute to div. Key should be on the highest level directly after map */}
+                  {/* TODO: Add key attribute to div. Key should be on the highest level directly after map */}
                   <ProductCard
                     key={index}
                     id={item.id}
                     name={item.name}
+                    description={item.description}
                     image={item.images[0].src.small}
                     price={item.price}
                   />
