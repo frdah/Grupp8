e41fe8b3588500ff9e2eed992643de6e99b55d44
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-04 17:59:07 +0200

    Bootstrap på review-korten

--- src/components/ProductReview.jsx
+++ src/components/ProductReview.jsx
@@ -12,12 +12,16 @@ export default function ProductReview({
 		<div className="row justify-content-md-center">
 			<div className="col-12 col-md-6 col-xl-6 mt-3">
 				<div className="card">
+					<div className="card-header bg-light">
+						<h5 className="card-title mb-0">{title}</h5>
+					</div>
 					<div className="card-body">
-						<h5 className="card-title">{title}</h5>
-						<p>{description}</p>
-						<p>Author: {author}</p>
-						<p>Date: {date}</p>
-						<p>Rating: {rating}</p>
+						<p className="">{description}</p>
+						<p>★ {rating} </p>
+						<div className="row">
+							<p className="col-8 font-weight-bold text-info">{author}</p>
+							<p className="col-4 font-italic">{date}</p>
+						</div>
 					</div>
 				</div>
 			</div>
