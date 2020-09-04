d4b58e4dbf78f7876a542de4839cdb71239cec24
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2020-09-04 18:50:24 +0200

    Lagt till responsivitet på startsidan

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,14 +11,14 @@ export default function ProductCard({
 }) {
 	const shortDescription = description.split(" ").slice(0, 10).join(" ")
 	return (
-		<div className="product-card card m-4 p-2 bg-light shadow" width="5vw">
-			<div className="card-body p-2">
+		<div className="product-card card my-3 mx-2 bg-light shadow">
+			<div className="card-body p-3">
 				<img className="card-img-top img-productlist" src={image} alt="" />
 				<h4 className="card-title mt-3">{name}</h4>
 				<p className="card-text">{shortDescription}...</p>
-				<h6 className="card-text">Price: {price} SEK</h6>
+				<h5 className="card-text">{price} SEK</h5>
 				<Link className="btn btn-info" to={`/products/${id}`}>
-					Show product
+					Read more
 				</Link>
 			</div>
 		</div>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,47 +1,47 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import ProductCard from "./ProductCard";
+import React from "react"
+import { useState, useEffect } from "react"
+import ProductCard from "./ProductCard"
 
 export default function ProductList() {
-  let [productList, setProductList] = useState({});
+	let [productList, setProductList] = useState({})
 
-  function fetchProductList() {
-    fetch("https://mock-data-api.firebaseio.com/e-commerce/products.json")
-      .then(res => res.json())
-      .then(result => {
-        console.log(result);
-        setProductList(result);
-      });
-  }
+	function fetchProductList() {
+		fetch("https://mock-data-api.firebaseio.com/e-commerce/products.json")
+			.then((res) => res.json())
+			.then((result) => {
+				console.log(result)
+				setProductList(result)
+			})
+	}
 
-  useEffect(() => {
-    fetchProductList();
-  }, []);
+	useEffect(() => {
+		fetchProductList()
+	}, [])
 
-  return (
-    <div>
-      <div className="container">
-        <div className="row">
-          {productList &&
-            Object.entries(productList).map((itemArray, index) => {
-              let item = itemArray[1];
+	return (
+		<div>
+			<div className="container">
+				<div className="row">
+					{productList &&
+						Object.entries(productList).map((itemArray, index) => {
+							let item = itemArray[1]
 
-              return (
-                <div className="col-sm-4">
-                  {/* TODO: Add key attribute to div. Key should be on the highest level directly after map */}
-                  <ProductCard
-                    key={index}
-                    id={item.id}
-                    name={item.name}
-                    description={item.description}
-                    image={item.images[0].src.small}
-                    price={item.price}
-                  />
-                </div>
-              );
-            })}
-        </div>
-      </div>
-    </div>
-  );
+							return (
+								<div className="col-12 col-md-6 col-xl-4">
+									{/* TODO: Add key attribute to div. Key should be on the highest level directly after map */}
+									<ProductCard
+										key={index}
+										id={item.id}
+										name={item.name}
+										description={item.description}
+										image={item.images[0].src.small}
+										price={item.price}
+									/>
+								</div>
+							)
+						})}
+				</div>
+			</div>
+		</div>
+	)
 }
